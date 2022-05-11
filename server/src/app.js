c9dba0674fb83f31442dafdca83f78737a97dc98
stelleal <stellalleal@gmail.com>
2022-05-11 17:17:49 -0300

    GET all launches

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -19,7 +20,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use(launchesRouter);
+app.get('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
