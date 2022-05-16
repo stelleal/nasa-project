b34c8f639c8f1db80d05b31e468c73d331a567b0
stelleal <stellalleal@gmail.com>
2022-05-16 19:12:02 -0300

    Add new launch validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,15 +5,26 @@ function httpGetAllLaunches(req, res) {
 }
 
 function httpAddNewLaunch(req, res) {
-  const launch = req.body;
+	const launch = req.body;
 
-  launch.launchDate = new Date(launch.launchDate);
+	if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+		return res.status(400).json({
+			error: 'Missing required launch property',
+		});
+	}
 
-  addNewLaunch(launch);
-  return res.status(201).json(launch);
+	launch.launchDate = new Date(launch.launchDate);
+	if (isNaN(launch.launchDate)) {
+		return res.status(400).json({
+			error: 'Invalid launch date',
+		});
+	}
+
+	addNewLaunch(launch);
+	return res.status(201).json(launch);
 }
 
 module.exports = {
 	httpGetAllLaunches,
-  httpAddNewLaunch,
+	httpAddNewLaunch,
 };
