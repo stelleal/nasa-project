daa29a3a6f0612479a181daffd73c57b72fd9937
stelleal <stellalleal@gmail.com>
2022-05-16 18:39:30 -0300

    Add new launch endpoint

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
 	flightNumber: 100,
 	mission: 'Kepler Exploration X',
@@ -7,8 +9,8 @@ const launch = {
 	launchDate: new Date('December 27, 2030'),
 	destination: 'Kepler-442 b',
 	customers: ['ZTM', 'NASA'],
-  upcoming: true,
-  success: true,
+	upcoming: true,
+	success: true,
 };
 
 launches.set(launch.flightNumber, launch);
@@ -17,6 +19,20 @@ function getAllLaunches() {
 	return Array.from(launches.values());
 }
 
-module.exports = {
-  getAllLaunches,
+function addNewLaunch(launch) {
+	latestFlightNumber++;
+	launches.set(
+		latestFlightNumber,
+		Object.assign(launch, {
+			flightNumber: latestFlightNumber,
+			customers: ['Zero To Mastery', 'NASA'],
+			upcoming: true,
+			success: true,
+		})
+	);
 }
+
+module.exports = {
+	getAllLaunches,
+	addNewLaunch,
+};
