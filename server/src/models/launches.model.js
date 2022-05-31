fdeae195e429af96c51ca4b9031ab49e2231f3b7
stelleal <stellalleal@gmail.com>
2022-05-31 19:02:11 -0300

    Sort paginated data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -92,6 +92,7 @@ async function getLatestFlightNumber() {
 async function getAllLaunches(skip, limit) {
 	return await launchesDB
 		.find({}, { _id: 0, __v: 0 })
+		.sort({ flightNumber: 1 })
 		.skip(skip)
 		.limit(limit);
 }
